37cba233a084150bc0c305117cf184658e0eaca4
ABertie <U2439307@rts-365.dk>
2023-10-13 10:53:56 +0200

    brian er __

--- webpack.config.js
+++ webpack.config.js
@@ -1,15 +1,20 @@
 const MiniCssExtractPlugin = require("mini-css-extract-plugin")
 const ImageMinimizerPlugin = require("image-minimizer-webpack-plugin")
+const HtmlWebpackPlugin = require("html-webpack-plugin")
+const path = require("path")
 
 module.exports = {
-	mode: process.env.environment == "production" ? "production" : "development",
-	plugins: [new MiniCssExtractPlugin()],
+	// mode: process.env.environment == "production" ? "production" : "development",
+	mode: "production",
+	plugins: [new MiniCssExtractPlugin(), new HtmlWebpackPlugin({
+		template: path.resolve(__dirname, "src", "index.html")
+	})],
 	module: {
 		rules: [
 			{
 				test: /\.(s[ac]|c)ss$/i,
 				//test: path.resolve(__dirname, "src/scss/*"),
-				use: [MiniCssExtractPlugin.loader, "css-loader", "sass-loader", "postcss-loader"]
+				use: [MiniCssExtractPlugin.loader, "css-loader", "sass-loader", "postcss-loader"],
 			},{
 				test: /\.png$/i,
 				type: "asset"
